8f4c00a39c21344f37a28d8d6b1761f589868507
Jaro Marval <jampgold@gmail.com>
2013-04-11 12:59:20 -0430

    Corrección parroquias

--- default/app/models/centro_votacion.php
+++ default/app/models/centro_votacion.php
@@ -8,7 +8,7 @@ class CentroVotacion extends ActiveRecord {
 
     public function listarCentro($municipio){
         $columns = "columns: id, cod_centro,centro_nuevo,nombre_centro,tipo_psuv";
-        $conditions = "conditions : WHERE municipio_id = $municipio";
+        $conditions = "conditions : municipio_id = $municipio";
         return $this->find($columns,$conditions);
     }
 
--- default/app/models/parroquia.php
+++ default/app/models/parroquia.php
@@ -6,9 +6,9 @@ class Parroquia extends ActiveRecord {
 
 
     public function listarParroquia($municipio){
-    $columns = "columns: id, parroquia";
-    $conditions = "conditions : WHERE municipio_id = $municipio";
-    return $this->find($columns,$conditions);
+        $columns = "columns: id, parroquia";
+        $conditions = "conditions: municipio_id = $municipio";
+        return $this->find($columns, $conditions);
 	}
 
 }
