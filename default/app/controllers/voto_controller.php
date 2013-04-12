86e01bf3cb35cd4580e2084e01adf228585c5b22
Ricardo A Maita <maitamricardo@gmail.com>
2013-04-12 00:57:33 -0430

    busqueda de Monito

--- default/app/controllers/voto_controller.php
+++ default/app/controllers/voto_controller.php
@@ -16,24 +16,23 @@ $this->mensaje ="Reportar Cedula / Voto";
 
 	public function buscar(){
 		Load::model('datos_personales');
-		if(Input::HasPost('buscar')){
-			print_r(Input::post('buscar'));
-			$variable = new DatosPersonales(Input::post('buscar'));
-			print_r($variable->buscar());
+		if(Input::HasPost('cedula')){
+			$variable = new DatosPersonales();
+			 $this->busqueda = $variable->buscar(Input::post('cedula'));
 
 				}
 
 			}
 
 
-		public function buscarvoto(){
-		Load::model('datos_personales');
-		if(Input::HasPost('buscar')){
-			print_r(Input::post('buscar'));
-			$variable = new DatosPersonales(Input::post('buscar'));
-			print_r($variable->buscar());
-				}
-		   }
+		// public function buscarvoto(){
+		// Load::model('datos_personales');
+		// if(Input::HasPost('buscar')){
+		// 	print_r(Input::post('buscar'));
+		// 	$variable = new DatosPersonales(Input::post('buscar'));
+		// 	print_r($variable->buscar());
+		// 		}
+		//    }
 
 
 
--- default/app/models/datos_personales.php
+++ default/app/models/datos_personales.php
@@ -3,14 +3,26 @@
 
 class DatosPersonales extends ActiveRecord{
 
-
+protected $logger = TRUE;
 
 	function buscar($cedula){
+
 		$cedula = Filter::get($cedula,'int');
-		return $this->find_first('cedula = '.$cedula);
+		 $columns = "columns:  datos_personales.cedula, datos_personales.nombres,
+		 			 datos_personales.apellidos , datos_personales.telefono,informacion.estatus,
+		 			 datos_personales.celular,centro_votacion.nombre_centro as centro_votacion";
 
-	}
+		 $join = "join: LEFT JOIN centro_votacion ON  centro_votacion.id = datos_personales.centro_votacion_id
+		 				LEFT JOIN informacion ON datos_personales.id = informacion.datos_personales_id";
+		$resultado = $this->find_first('cedula = '.$cedula,$columns, $join);
 
+		if($resultado){
+			return $resultado;
+
+			}else{
+				return false;
+			}
+		}
 }
 
 
