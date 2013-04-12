08387e9d0ca07cc63a760633b1f95ac6f048af68
Jaro Marval <jampgold@gmail.com>
2013-04-11 23:26:26 -0430

    verificación de repetidos

--- default/app/models/reporte_pdvsa.php
+++ default/app/models/reporte_pdvsa.php
@@ -3,12 +3,17 @@
 class ReportePdvsa extends ActiveRecord {
 
     public function insertar($cedula) {
-        $this->cedula = $cedula;
-        $this->ip = Utils::getIp();
-        if ( $this->create() ) {
-            return True;
-        } else {
+        $cedula = Filter::get($cedula, 'int');
+        if ( $this->exists('cedula = '.$cedula) ) {
             return False;
+        }  else {
+            $this->cedula = $cedula;
+            $this->ip = Utils::getIp();
+            if ( $this->create() ) {
+                return True;
+            } else {
+                return False;
+            }
         }
     }
 }
