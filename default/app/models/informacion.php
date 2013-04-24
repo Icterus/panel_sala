58bdaeca4f4cdd4191c9543523961aa91f248f2c
Jaro Marval <jampgold@gmail.com>
2013-04-24 13:29:33 -0430

    Agregando voto azul

--- default/app/models/informacion.php
+++ default/app/models/informacion.php
@@ -4,12 +4,12 @@
 class Informacion extends ActiveRecord{
     public $logger = true;
 
-    public function registrarVoto($id) {
+    public function registrarVoto($id, $azul=False) {
         $id = Filter::get($id, 'int');
         $persona = Load::model('datos_personales')->buscarCentro($id);
         if ( $persona ) {
             $this->datos_personales_id = $id;
-            $this->voto = 1;
+            $this->voto = ($azul)?'0':'1';
             $this->centro_votacion_id = $persona->centro;
             $this->usuario_id = Auth::get('id');
             if ( $this->create() ) return True;
