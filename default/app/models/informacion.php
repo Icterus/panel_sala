b94ac770706c03363ffeedf318f0d727917fa748
Jaro Marval <jampgold@gmail.com>
2013-04-25 12:08:33 -0430

    Corrección de voto azul

--- default/app/models/informacion.php
+++ default/app/models/informacion.php
@@ -9,7 +9,7 @@ class Informacion extends ActiveRecord{
         $persona = Load::model('datos_personales')->buscarCentro($id);
         if ( $persona ) {
             $this->datos_personales_id = $id;
-            $this->voto = ($azul)?'0':'1';
+            $this->voto = (!$azul)?'0':'1';
             $this->centro_votacion_id = $persona->centro;
             $this->usuario_id = Auth::get('id');
             if ( $this->create() ) return True;
