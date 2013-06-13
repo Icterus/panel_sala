06bd0db9b1b47824f467ac38421e6b25b9f4c021
Ricardo A Maita <maitamricardo@gmail.com>
2013-06-13 16:15:49 -0430

    agregado el campo + en buscar para establecer modulo de consulta

--- default/app/models/datos_personales.php
+++ default/app/models/datos_personales.php
@@ -10,10 +10,12 @@ protected $logger = TRUE;
 	$cedula = Filter::get($cedula,'int');
 	 $columns = "columns: datos_personales.id, datos_personales.cedula, datos_personales.nombres,
 	 			 datos_personales.apellidos , datos_personales.telefono,informacion.estatus,informacion.voto,
-	 			 datos_personales.celular, centro_votacion.id AS centro_id, centro_votacion.nombre_centro as centro_votacion";
+	 			 datos_personales.celular, centro_votacion.id AS centro_id, centro_votacion.nombre_centro as centro_votacion
+	 			 militancia.psuv as psuv";
 
 	 $join = "join: LEFT JOIN centro_votacion ON  centro_votacion.id = datos_personales.centro_votacion_id
-	 				LEFT JOIN informacion ON datos_personales.id = informacion.datos_personales_id";
+	 				LEFT JOIN informacion ON datos_personales.id = informacion.datos_personales_id
+	 				LEFT JOIN militancia ON datos_personales.id = militancia.datos_personales_id ";
 	$resultado = $this->find_first('cedula = '.$cedula, $columns, $join);
 
 	if($resultado){
